d9be2b194a97e89a255e600ee6280e214c4490d7
Matthew Nelson <matthewbuzz@gmail.com>
2025-10-18 09:17:08 -0600

    Add login/sign out button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import logoImage from '/logo.png';
-import { useAuth } from '../contexts/AuthContext';
+import AuthSection from './AuthSection';
 
 const Header: React.FC = () => {
-  const { user, signOut } = useAuth();
-
   return (
     <header className="header">
       <nav className="container">
@@ -15,28 +13,11 @@ const Header: React.FC = () => {
         <ul className="nav-links">
           <li><NavLink to="/features">Features</NavLink></li>
           <li><NavLink to="/how-it-works">How It Works</NavLink></li>
-          {user && (
-            <>
-              <li><NavLink to="/employer">Employer Dashboard</NavLink></li>
-              <li><NavLink to="/candidates">Candidates</NavLink></li>
-            </>
-          )}
-          {!user && (
-            <>
-              <li><NavLink to="/login">Login</NavLink></li>
-              <li><NavLink to="/register">Register</NavLink></li>
-            </>
-          )}
+          <li><NavLink to="/employer">Employer Dashboard</NavLink></li>
+          <li><NavLink to="/candidates">Candidates</NavLink></li>
           <li><NavLink to="/testing">Vector Testing</NavLink></li>
         </ul>
-        {user ? (
-          <div className="auth-section">
-            <span>Welcome, {user.email}</span>
-            <button onClick={signOut} className="cta-button">Sign Out</button>
-          </div>
-        ) : (
-          <NavLink to="/login" className="cta-button">Login</NavLink>
-        )}
+        <AuthSection />
       </nav>
     </header>
   );
